c75a45c3b8f68ef9f0baa5c5239bfa1f2f465b29
mumba <mumba@DESKTOP-TG3CG74>
2024-09-25 18:46:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace ConsoleApp16
+{
+    class Dict
+    {
+        Dictionary<string, string> words = new Dictionary<string, string>()
+        {
+            {"Желтый",  "Yellow"}, {"Синий", "Blue"}, {"Красный", "Red"}
+        };
+
+       
+        public void addTranslate(string word, string translate)
+        {
+            if (!words.ContainsKey(word))
+            {
+                words.Add(word, translate);
+            }
+          
+
+            foreach (KeyValuePair<string, string> pair in words)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+       
+        public void removeWord(string word)
+        {
+            if (words.ContainsKey(word))
+            {
+                words.Remove(word);
+            }
+           
+
+            foreach (KeyValuePair<string, string> pair in words)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+        
+        public void removeTranslate(string word)
+        {
+            if (words.Remove(word))
+            {
+                Console.WriteLine($"Removed {word}.");
+            }
+           
+
+            foreach (KeyValuePair<string, string> pair in words)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+       
+        public void changeTranslate(string key, string newTranslate)
+        {
+            if (words.ContainsKey(key))
+            {
+                words[key] = newTranslate; 
+            }
+          
+
+            foreach (KeyValuePair<string, string> pair in words)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+       
+        public void searchTranslate(string key)
+        {
+            if (words.TryGetValue(key, out string value))
+            {
+                Console.WriteLine($"{key}: {value}");
+            }
+           
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dict obj = new Dict();
+            obj.addTranslate("Фиолетовый", "Purple");  
+            obj.removeTranslate("Красный");  
+            obj.changeTranslate("Синий", "White");  
+            obj.searchTranslate("Желтый");  
+        }
+    }
+}
+
